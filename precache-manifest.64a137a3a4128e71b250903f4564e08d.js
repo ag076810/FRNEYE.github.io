7e1740ee834a50b8e4febd2b0fb2dfa5a3f3ab55
ag076810 <88880369+ag076810@users.noreply.github.com>
2022-01-23 01:41:13 +0800

    Add files via upload

---
+++ precache-manifest.64a137a3a4128e71b250903f4564e08d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d46cb35d1e40748a0e6035a39adf44d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "43346937df3b9badc9d9",
+    "url": "/static/css/main.de453e59.chunk.css"
+  },
+  {
+    "revision": "c7430866e2d257b24df1",
+    "url": "/static/js/2.b6c54f85.chunk.js"
+  },
+  {
+    "revision": "306907fccee50102c62f",
+    "url": "/static/js/3.4896f7ca.chunk.js"
+  },
+  {
+    "revision": "43346937df3b9badc9d9",
+    "url": "/static/js/main.58b444cd.chunk.js"
+  },
+  {
+    "revision": "5a497db0d7d0bf2e4fcb",
+    "url": "/static/js/runtime~main.b3e4c5be.js"
+  },
+  {
+    "revision": "e63562614cbdc491e4d374f97c513b82",
+    "url": "/static/media/font.e6356261.woff"
+  }
+]);
